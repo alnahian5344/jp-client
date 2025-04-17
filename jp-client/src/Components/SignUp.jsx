f5387d16116e35b564e15166de1db54cc2e6cf4d
alnahian5344 <alnahian5344@gmail.com>
2025-04-17 16:28:05 +0600

    SETUP DONE

--- jp-client/src/Components/SignUp.jsx
+++ jp-client/src/Components/SignUp.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const SignUP = () => {
+    const handleSignUp = e => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const phone = form.phone.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        const user = { name, phone, email, password };
+        console.log(user);
+    }
     return (
+
         <div>
             <div className="hero bg-base-200 min-h-[650px]">
                 <div className="hero-content flex flex-col">
@@ -10,24 +21,23 @@ const SignUP = () => {
                     </div>
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                         <div className="card-body">
-                            <form action="">
+                            <form onSubmit={handleSignUp}>
                                 <fieldset className="fieldset">
                                     <label className="fieldset-label">Name</label>
-                                    <input type="text" value="name" className="input" placeholder="Name" />
+                                    <input type="text" name='name' className="input" placeholder="Name" />
 
                                     <label className="fieldset-label">Phone</label>
-                                    <input type="text" value="phone" className="input" placeholder="Phone" />
+                                    <input type="text" name='phone' className="input" placeholder="Phone" />
 
                                     <label className="fieldset-label">Email</label>
-                                    <input type="email" value="email" className="input" placeholder="Email" />
+                                    <input type="email" name='email' className="input" placeholder="Email" />
 
                                     <label className="fieldset-label">Password</label>
-                                    <input type="password" value="password" className="input" placeholder="Password" />
+                                    <input type="password" name='password' className="input" placeholder="Password" />
                                     <button className="btn btn-neutral mt-4">Sign Up</button>
                                 </fieldset>
                             </form>
                         </div>
-
                     </div>
                 </div>
 
